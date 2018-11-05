ed532c06540ecc4e3edc1c3abcf7b09415adf349
Javier <Javier@DarkLevel>
2018-11-05 10:25:32 +0100

    Carpetas hechas

--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -1,10 +1,16 @@
 trolleyes.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {templateUrl:'js/app/common/home.html', controller: 'homeController'});
+        $routeProvider.when('/factura/plist/', {templateUrl:'js/app/factura/plist.html', controller: 'facturaPlistController'});
         $routeProvider.when('/factura/plist/:rpp?/:page?', {templateUrl:'js/app/factura/plist.html', controller: 'facturaPlistController'});
+        $routeProvider.when('/linea/plist/', {templateUrl:'js/app/linea/plist.html', controller: 'lineaPlistController'});
         $routeProvider.when('/linea/plist/:rpp?/:page?', {templateUrl:'js/app/linea/plist.html', controller: 'lineaPlistController'});
+        $routeProvider.when('/producto/plist/', {templateUrl:'js/app/producto/plist.html', controller: 'prodcutoPlistController'});
         $routeProvider.when('/producto/plist/:rpp?/:page?', {templateUrl:'js/app/producto/plist.html', controller: 'prodcutoPlistController'});
+        $routeProvider.when('/tipoproducto/plist/', {templateUrl:'js/app/tipoproducto/plist.html', controller: 'tipoproductoPlistController'});
         $routeProvider.when('/tipoproducto/plist/:rpp?/:page?', {templateUrl:'js/app/tipoproducto/plist.html', controller: 'tipoproductoPlistController'});
+        $routeProvider.when('/usuario/plist/', {templateUrl:'js/app/usuario/plist.html', controller: 'usuarioPlistController'});
         $routeProvider.when('/usuario/plist/:rpp?/:page?', {templateUrl:'js/app/usuario/plist.html', controller: 'usuarioPlistController'});
+        $routeProvider.when('/tipousuario/plist/', {templateUrl:'js/app/tipousuario/plist.html', controller: 'tipousuarioPlistController'});
         $routeProvider.when('/tipousuario/plist/:rpp?/:page?', {templateUrl:'js/app/tipousuario/plist.html', controller: 'tipousuarioPlistController'});
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
