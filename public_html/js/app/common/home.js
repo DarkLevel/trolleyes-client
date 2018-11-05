1db986840bc70b0ae488ab12c22172fd5ba03be9
Javier <Javier@DarkLevel>
2018-11-05 11:08:41 +0100

    Cargar js en index

--- public_html/js/app/common/home.js
+++ public_html/js/app/common/home.js
@@ -6,5 +6,4 @@ moduleCommon.controller('homeController', ['$scope', '$location', 'toolService',
         $scope.ruta = $location.path();
 
         $scope.isActive = toolService.isActive;
-
     }]);
