7687ebf51c934e6941d8ebf7d3a0129a2cba37b7
Javier <Javier@DarkLevel>
2018-11-06 01:46:10 +0100

    Problema filas tabla

--- public_html/js/app/producto/plist.js
+++ public_html/js/app/producto/plist.js
@@ -1,54 +1,69 @@
-'use strict'
+/* global moduleProducto */
+
+'use strict';
 
 moduleProducto.controller('productoPlistController', ['$scope', '$http', '$location', 'toolService', '$routeParams',
     function ($scope, $http, $location, toolService, $routeParams) {
-        $scope.pagina = "1";
-        $scope.registrosPagina = "5";
-        $scope.mostrar = false;
-        $scope.activar = true;
+        $scope.totalPages = 1;
+        if (!$routeParams.rpp) {
+            $scope.rpp = 10;
+        } else {
+            $scope.rpp = $routeParams.rpp;
+        }
+        if (!$routeParams.page) {
+            $scope.page = 1;
+        } else {
+            if ($routeParams.page >= 1) {
+                $scope.page = $routeParams.page;
+            } else {
+                $scope.page = 1;
+            }
+        }
+        if (!$routeParams.column) {
+            $scope.column = 1;
+        } else {
+            $scope.column = $routeParams.column;
+        }
+        if (!$routeParams.order) {
+            $scope.order = 'asc';
+        } else {
+            $scope.order = $routeParams.order;
+        }
 
-        $scope.tabla = function () {
-            $scope.pagina = "1";
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getcount'
+        }).then(function (response) {
+            $scope.status = response.status;
+            $scope.ajaxDataNumero = response.data.message;
+            $scope.totalPages = Math.ceil($scope.ajaxDataNumero / $scope.rpp);
+            $scope.list = [];
+            for (var i = 1; i <= $scope.totalPages; i++) {
+                $scope.list.push(i);
+            }
+        }, function (response) {
+            $scope.ajaxDataNumero = response.data.message || 'Request failed';
+            $scope.status = response.status;
+        });
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=' + $scope.rpp + '&page=' + $scope.page + '&column=' + $scope.column + '&order=' + $scope.order
+        }).then(function (response) {
+            $location.url('producto/plist/' + $scope.rpp + '/' + $scope.page + '/' + $scope.column + '/' + $scope.order);
+            $scope.status = response.status;
+            $scope.ajaxData = response.data.message;
+        }, function (response) {
+            $scope.ajaxData = response.data.message || 'Request failed';
+            $scope.status = response.status;
+        });
+
+        $scope.cambiarRegistros = function () {
             $http({
                 method: 'GET',
-                withCredentials: true,
-                url: "http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=" + $scope.registrosPagina + "&page=" + $scope.pagina
+                url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=' + $scope.rpp + '&page=1&column=1&order=asc'
             }).then(function (response) {
-                $scope.status = response.status;
-                $scope.ajaxData = response.data.message;
-            }, function (response) {
-                $scope.ajaxData = response.data.message || 'Request failed';
-                $scope.status = response.status;
-            });
-            $http({
-                method: 'GET',
-                withCredentials: true,
-                url: "http://localhost:8081/trolleyes/json?ob=producto&op=getcount"
-            }).then(function (response) {
-                $scope.status = response.status;
-                $scope.numeroRegistros = response.data.message;
-                $scope.numeroPaginas = Math.ceil($scope.numeroRegistros / $scope.registrosPagina);
-                $scope.arrayPages = [];
-                for (var i = 1; i <= $scope.numeroPaginas; i++) {
-                    $scope.arrayPages.push(i);
-                }
-                if (!$scope.mostrar) {
-                    $scope.mostrar = !$scope.mostrar;
-                }
-                if ($scope.activar) {
-                    $scope.activar = !$scope.activar;
-                }
-            }, function (response) {
-                $scope.numeroRegistros = response.data.message || 'Request failed';
-                $scope.status = response.status;
-            });
-        };
-        $scope.elegirPagina = function () {
-            $http({
-                method: 'GET',
-                withCredentials: true,
-                url: "http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=" + $scope.registrosPagina + "&page=" + $scope.pagina
-            }).then(function (response) {
+                $location.url('producto/plist/' + $scope.rpp + '/1/1/asc');
                 $scope.status = response.status;
                 $scope.ajaxData = response.data.message;
             }, function (response) {
@@ -56,15 +71,20 @@ moduleProducto.controller('productoPlistController', ['$scope', '$http', '$locat
                 $scope.status = response.status;
             });
         };
-        $scope.limpiarTabla = function () {
-            $scope.pagina = "1";
-            $scope.registrosPagina = "5";
-            if ($scope.mostrar) {
-                $scope.mostrar = !$scope.mostrar;
-            }
-            if (!$scope.activar) {
-                $scope.activar = !$scope.activar;
-            }
+
+        $scope.ordenar = function (column, order) {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=' + $scope.rpp + '&page=1&column=' + column + '&order=' + order
+            }).then(function (response) {
+                $location.url('producto/plist/' + $scope.rpp + '/1/' + column + '/' + order);
+                $scope.status = response.status;
+                $scope.ajaxData = response.data.message;
+            }, function (response) {
+                $scope.ajaxData = response.data.message || 'Request failed';
+                $scope.status = response.status;
+            });
         };
+
         $scope.isActive = toolService.isActive;
     }]);
--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -11,6 +11,7 @@ trolleyes.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/producto/view/:id?', {templateUrl:'js/app/producto/view.html', controller: 'productoViewController'});
         $routeProvider.when('/producto/plist/', {templateUrl:'js/app/producto/plist.html', controller: 'productoPlistController'});
         $routeProvider.when('/producto/plist/:rpp?/:page?', {templateUrl:'js/app/producto/plist.html', controller: 'productoPlistController'});
+        $routeProvider.when('/producto/plist/:rpp?/:page?/:column?/:order?', {templateUrl:'js/app/producto/plist.html', controller: 'productoPlistController'});
         
         $routeProvider.when('/tipoproducto/view/:id?', {templateUrl:'js/app/tipoproducto/view.html', controller: 'tipoproductoViewController'});
         $routeProvider.when('/tipoproducto/plist/', {templateUrl:'js/app/tipoproducto/plist.html', controller: 'tipoproductoPlistController'});
